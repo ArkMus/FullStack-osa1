3f2b7545d485ac1ecaafd0e2344dbe26cc9abf78
Arkmus <markus.andersson97@gmail.com>
2019-11-09 12:45:18 +0200

    unicafe done! (i.e. up to step 6 or 1.10 done)

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -16,7 +16,7 @@ const averagePos = function(a, b) {
   if (b === 0) {
     return 0
   } 
-  return (a / b) * 100
+  return ((a / b) * 100) +"%"
 }
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -27,7 +27,6 @@ const Statistics = ({ good, neutral, bad }) => {
   if(cA === 0) {
     return (
       <div>
-        <h1>statistics</h1>
         <p>No feedback given</p>
       </div>
     )
@@ -35,41 +34,81 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return(
     <div>
-    <h1>statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {cA}</p>
-    <p>average {aR}</p>
-    <p>positive {aP}%</p>
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td><Statistic value={good}/></td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td><Statistic value={neutral}/></td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td><Statistic value={bad}/></td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td><Statistic value={cA}/></td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td><Statistic value={aR}/></td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td><Statistic value={aP}/></td>
+          </tr>
+        </tbody>
+      </table>
   </div>
   )
 }
 
+const Statistic = (props) => {
+  return (
+    <div>
+      <p>{props.value} </p>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.clickHandler}>{props.text}</button>
+    </div>
+  )
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const goodHandler = () => {
+    setGood(good + 1)
+  }
+
+  const neutralHandler = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const badHandler = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
 
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
+      <Button clickHandler={goodHandler} text="good"/>
+      <Button clickHandler={neutralHandler} text="neutral"/>
+      <Button clickHandler={badHandler} text="bad"/>
 
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
-
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad}/>
-
     </div>
   )
 }
